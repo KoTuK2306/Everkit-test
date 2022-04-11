9ec187280264affa453b0555d4226644b9e5b1ef
KoTuK2306 <dmitriy_aleshin_00@mail.ru>
2022-04-11 21:45:22 +0300

    fix: fixed routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,19 @@ import { FC } from 'react'
 import { Menu } from './components/Menu'
 import { Tokens } from './components/Tokens'
 import { Search } from './components/Search'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import classes from './App.module.css'
 
 export const App: FC = () => {
   return (
     <section className={classes.app}>
-      <BrowserRouter>
+      <HashRouter>
         <Menu />
         <Routes>
           <Route index element={<Tokens />} />
           <Route path="/search" element={<Search />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </section>
   )
 }
